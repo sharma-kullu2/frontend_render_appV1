717f0c70c140e0c67b846f5dd9ff20c259ec7a71
sharma-kullu2 <er.gaurav.droid1@gmail.com>
2023-05-08 13:57:07 +0530

    1.2 add gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_API_ENDPOINT)
+      .then(response => {
+        this.setState({ data: response.data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        {data ? (
+          <div>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+          </div>
+        ) : (
+          <p>Loading data...</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
