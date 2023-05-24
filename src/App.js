9a705a079dfe698457232e227e7eea405079504f
sharma-kullu2 <er.gaurav.droid1@gmail.com>
2023-05-24 13:44:59 +0530

    Revert "1.3 Added mui/ Blog to src code"
    This reverts commit 4207b6cf044cac1b514a589694aa20317886d57a.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Blog from './components/Blog/Blog'
-import Nav from './components/Blog/Nav'
+import axios from 'axios';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
 
-export default function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Blog />} />
-        <Route path="/nav" element={<Nav />} />
-      </Routes>
-    </div>
-  );
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_API_ENDPOINT)
+      .then(response => {
+        this.setState({ data: response.data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        {data ? (
+          <div>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+          </div>
+        ) : (
+          <p>Loading data...</p>
+        )}
+      </div>
+    );
+  }
 }
+
+export default App;
